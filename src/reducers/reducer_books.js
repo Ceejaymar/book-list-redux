7936a4531f85c41c798ce2593080b3b391dd1fba
Carlos Martinez <martinezcj2@gmail.com>
2017-09-05 16:13:12 -0400

    Create books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: the good'},
+    {title: 'Eloquent Javascript'},
+    {title: 'The Demon haunted World'},
+    {title: 'Brave new world'}
+  ]  
+}
