c87af9450cb6665f2b79752406ea35b3a280f2cf
Carlos Martinez <martinezcj2@gmail.com>
2017-09-05 21:33:47 -0400

    Add page information to books

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript: the good'},
-    {title: 'Eloquent Javascript'},
-    {title: 'The Demon haunted World'},
-    {title: 'Brave new world'}
+    {title: 'Javascript: the good', pages: 101},
+    {title: 'Eloquent Javascript', pages: 85},
+    {title: 'The Demon haunted World', pages: 96},
+    {title: 'Brave new world', pages: 102}
   ]  
 }
